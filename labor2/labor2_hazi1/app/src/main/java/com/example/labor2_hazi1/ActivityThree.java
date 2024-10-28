f96c758c56b2022bf447456d696eec795f87887d
Aron <csatabedearonka@gmail.com>
2024-10-28 21:52:28 +0200

    labor2_hazi1

--- labor2/labor2_hazi1/app/src/main/java/com/example/labor2_hazi1/ActivityThree.java
+++ labor2/labor2_hazi1/app/src/main/java/com/example/labor2_hazi1/ActivityThree.java
@@ -1,59 +1,52 @@
 package com.example.labor2_hazi1;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityThree extends AppCompatActivity {
     private static final String TAG = "ActivityLifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d(TAG, "MainActivity - onCreate called");
+        setContentView(R.layout.activity_three);
+        Log.d(TAG, "ActivityThree - onCreate called");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "MainActivity - onStart called");
+        Log.d(TAG, "ActivityThree - onStart called");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "MainActivity - onResume called");
+        Log.d(TAG, "ActivityThree - onResume called");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "MainActivity - onPause called");
+        Log.d(TAG, "ActivityThree - onPause called");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "MainActivity - onStop called");
+        Log.d(TAG, "ActivityThree - onStop called");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "MainActivity - onRestart called");
+        Log.d(TAG, "ActivityThree - onRestart called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "MainActivity - onDestroy called");
-    }
-
-    public void startActivityTwo(View view) {
-        Intent intent = new Intent(this, ActivityTwo.class);
-        startActivity(intent);
+        Log.d(TAG, "ActivityThree - onDestroy called");
     }
 }
--- labor2/labor2_hazi1/app/src/main/java/com/example/labor2_hazi1/ActivityTwo.java
+++ labor2/labor2_hazi1/app/src/main/java/com/example/labor2_hazi1/ActivityTwo.java
@@ -6,54 +6,54 @@ import android.util.Log;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityTwo extends AppCompatActivity {
     private static final String TAG = "ActivityLifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d(TAG, "MainActivity - onCreate called");
+        setContentView(R.layout.activity_two);
+        Log.d(TAG, "ActivityTwo - onCreate called");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "MainActivity - onStart called");
+        Log.d(TAG, "ActivityTwo - onStart called");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "MainActivity - onResume called");
+        Log.d(TAG, "ActivityTwo - onResume called");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "MainActivity - onPause called");
+        Log.d(TAG, "ActivityTwo - onPause called");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "MainActivity - onStop called");
+        Log.d(TAG, "ActivityTwo - onStop called");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "MainActivity - onRestart called");
+        Log.d(TAG, "ActivityTwo - onRestart called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "MainActivity - onDestroy called");
+        Log.d(TAG, "ActivityTwo - onDestroy called");
     }
 
-    public void startActivityTwo(View view) {
-        Intent intent = new Intent(this, ActivityTwo.class);
+    public void startActivityThree(View view) {
+        Intent intent = new Intent(this, ActivityThree.class);
         startActivity(intent);
     }
 }
